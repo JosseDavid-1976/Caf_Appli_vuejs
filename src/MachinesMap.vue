253328c587f9c6a6d8dbf8813b133d0e899e4cb2
david <david.josse@campus-numerique-in-the-alps.com>
2018-03-07 16:18:38 +0100

    probléme google map mis en commentaire

--- src/MachinesMap.vue
+++ src/MachinesMap.vue
@@ -1,12 +1,20 @@
 <template>
     <div>
     <h1>Cartes Des Machine Du monde du Café</h1>
+
+        <!--<gmap-map-->
+                <!--:center="{lat:10, lng:10}"-->
+                <!--:zoom="7"-->
+                <!--style="width: 100%; height: 800px"-->
+        <!--&gt;</gmap-map>-->
+
     </div>
+
 </template>
 
 <script>
     export default {
-        name: "MachinesMap"
+        // name: "MachinesMap"
     }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -6,13 +6,23 @@ import MachinesList from './MachinesList.vue'
 import MachinesMap from './MachinesMap.vue'
 import MachineMachine from './MachineMachine.vue'
 
-Vue.component("listemachine",MachineMachine);
-Vue.component("listesmachines",MachinesList);
+// import * as VueGoogleMaps from 'vue2-google-maps'
+//
+// Vue.use(VueGoogleMaps, {
+//     load: {
+//         key: ' AIzaSyBQQZfVe4ThMQ8AO_Vd2ywLFi6314n6wVA  ',
+//     }
+// });
 
 Vue.use(VueRouter);
 
+Vue.component("listemachine",MachineMachine);
+Vue.component("listesmachines",MachinesList);
+Vue.component("gmap-map",MachinesMap);
+
+
 const routes = [
-    // {path: '/list', component: MachineMachine},
+
     {path: '/list', component: MachinesList},
     {path: '/map', component: MachinesMap},
     {path: '/machine', component:MachineMachine}
@@ -27,8 +37,18 @@ const app = new Vue({
 }).$mount('#app');
 
 
+
+
 new Vue({
     el: '#app',
     router,
-    render: h => h(App)
+
+    render: h => h(App),
+
+    // watch: {
+    //     '$route'(to, from) {
+    //         // Call resizePreserveCenter() on all maps
+    //         Vue.$gmapDefaultResizeBus.$emit('resize')
+    //     }
+    // }
 });
